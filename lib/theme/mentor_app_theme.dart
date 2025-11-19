7ded7d7f996bb8a21729e662a789db2d060e724d
ae-ronnn <singsonaeron0407@gmail.com>
2025-11-19 18:21:27 +0800

    removing comments

--- lib/theme/mentor_app_theme.dart
+++ lib/theme/mentor_app_theme.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
 
-/// TURO App Theme — follows your brand book
 class AppTheme {
   // 🎨 Brand Colors
-  static const Color primary = Color(0xFF104038); // Main brand color
-  static const Color secondary = Color(0xFF3D3D3D); // Dark text/neutral
-  static const Color grey = Color(0xFFD9D9D9); // Neutral
-  static const Color lightGrey = Color(0xFFF4F4F4); // Background
-  static const Color alert = Color(0xFFFF3E3E); // Red alert
+  static const Color primary = Color(0xFF104038);
+  static const Color secondary = Color(0xFF3D3D3D);
+  static const Color grey = Color(0xFFD9D9D9);
+  static const Color lightGrey = Color(0xFFF4F4F4);
+  static const Color alert = Color(0xFFFF3E3E);
   static const Color white = Color(0xFFFEFEFE);
   static const Color black = Color(0xFF3D3D3D);
-
-  // 🟢 TURO’s accent chip color (used in tags & budget)
   static const Color chipGreen = Color(0xFF2C6A64);
 
-  // ✍️ Text Styles (Fustat default)
   static const TextStyle fustatHeader = TextStyle(
     fontFamily: 'Fustat',
     fontWeight: FontWeight.w800,
@@ -50,7 +46,6 @@ class AppTheme {
     color: black,
   );
 
-  // --- Montserrat styles for specific requirements ---
   static const TextStyle montserratName = TextStyle(
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w600, // SemiBold
@@ -66,7 +61,6 @@ class AppTheme {
     color: primary,
   );
 
-  // 🌈 App ThemeData (for MaterialApp)
   static ThemeData get themeData {
     return ThemeData(
       useMaterial3: true,
